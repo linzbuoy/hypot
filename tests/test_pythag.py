19054dc31774db90fc267d9b9867594c83e7f2e0
Lynette Linzbuoy <81986840+linzbuoy@users.noreply.github.com>
2024-10-14 14:27:20 +0000

    Initial functions added and passing tests

--- tests/test_pythag.py
+++ tests/test_pythag.py
@@ -33,3 +33,74 @@ def test_add_nums_arrays():
 
     # Assert
     assert np.allclose(expected_output, output)
+    
+    
+def test_square_num():
+    '''Tests the function that adds two numbers'''
+
+    # Arrange
+    test_var_1 = 7
+    expected_output = 49
+
+    # Act
+    output = hypot.pythag.square_number(test_var_1)
+
+    # Assert
+    assert output == expected_output
+    
+def test_square_root():
+    '''Tests the function that adds two numbers'''
+
+    # Arrange
+    test_var_1 = 16
+    expected_output = 4
+
+    # Act
+    output = hypot.pythag.square_root(test_var_1)
+
+    # Assert
+    assert output == expected_output
+    
+def test_square_root_array():
+    '''Tests the function that adds two numbers'''
+
+    # Arrange
+    test_var_2 = np.array([16, 49, 64])
+    expected_output = np.array([4, 7, 8])
+
+    # Act
+    output = hypot.pythag.square_root(test_var_2)
+
+    # Assert
+    assert np.allclose(expected_output, output)
+    
+def test_square_root():
+    '''Tests the function that adds two numbers'''
+
+    # Arrange
+    test_var_1 = 16
+    expected_output = np.sqrt(16)
+
+    # Act
+    output = hypot.pythag.square_root(test_var_1)
+
+    # Assert
+    assert output == expected_output
+    
+def test_calc_hypot():
+    test_var_1 = 5.6
+    test_var_2 = 7.8
+    expected_output = 9.6
+    
+    output = hypot.pythag.calc_hypot(test_var_1,test_var_2 )
+    
+    assert np.allclose(expected_output, output, atol=1e-2)
+    
+def test_calc_hypot():
+    test_var_1 = np.array([10, 30045, 6.9, 0.04])
+    test_var_2 = np.array([23, 2400, 8.9, 0.34])
+    expected_output = np.array([25.08, 30140.7, 11.26, 0.34])
+    
+    output = hypot.pythag.calc_hypot(test_var_1, test_var_2)
+    
+    assert np.allclose(expected_output, output, rtol=1e-1)
